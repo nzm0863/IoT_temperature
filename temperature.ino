694d306d5001c8c0b34512c8c0521fa702049f90
nakamura <nzmtomjerry1245@gmail.com>
2026-07-13 10:26:09 +0900

    first commit

---
+++ temperature.ino
@@ -0,0 +1,30 @@
+#include <DHT.h>
+
+#define DHTPIN 5       // GPIO5
+#define DHTTYPE DHT22  // DHT22なら DHT22 に変更
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  Serial.println("DHT Test Start");
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("読み取り失敗");
+  } else {
+    Serial.print("温度: ");
+    Serial.print(temperature);
+    Serial.print(" °C\t湿度: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  }
+
+  delay(2000);
+}
